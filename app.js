dd8e2e6156b9a3f427ecded69d95203be7e2fd3b
JWanyoa <joel@kwelicapital.com>
2022-08-05 11:06:38 +0300

    Random User with Function

--- app.js
+++ app.js
@@ -29,8 +29,15 @@ const { createApp } = Vue
             }
         },
         methods: {
-            getUser(){
-                alert(this.firstName)
+            async getUser(){
+                const res = await fetch("https://randomuser.me/api")
+                const { results } = await res.json()
+                
+                this.firstName = results[0].name.first
+                this.lastName = results[0].name.last
+                this.email = results[0].email
+                this.gender = results[0].gender
+                this.picture = results[0].picture.large
             }
         }
     })
